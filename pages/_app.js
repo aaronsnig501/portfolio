5f171456b93f197dc73eba9e32dd78e7e8f0877b
Aaron Sinnott <aaronsnig@gmail.com>
2020-06-10 21:57:11 -0300

    Remove react-intl

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,5 @@
-import {useState, useEffect}  from 'react'
-import {IntlProvider} from 'react-intl'
 import '../styles/global.scss'
 
-import locale_en from '../translations/en.json'
-import locale_pt from '../translations/pt.json'
-
-const data = {
-  "en": locale_en,
-  "pt": locale_pt
-};
-
 export default function App({ Component, pageProps }) {
-  const [language, setLanguage] = useState('en')
-
-  useEffect(() => {
-    if (localStorage.getItem('languagePreference')) {
-      setLanguage(localStorage.getItem('languagePreference'))
-    } else {
-      setLanguage(navigator.language.split(/[-_]/)[0])
-    }
-  })
-
-  return (
-    <IntlProvider locale={language} messages={data[language]}>
-      <Component {...pageProps} />
-    </IntlProvider>
-  )
+  return <Component {...pageProps} />
 }
