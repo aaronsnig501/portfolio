5f4ba5058fe8a3ec0ee722fdb024b78070333e9a
Aaron Sinnott <aaronsnig@gmail.com>
2020-06-12 12:34:09 -0300

    Refactor Navigation component to function-based compoent

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,56 +1,47 @@
 import React from "react";
 
-export default class NavigationBar extends React.Component {
+export default function NavigationBar() {
+  const [navText, setNavText] = React.useState('ls')
 
-  componentDidMount = () => {
-    
-  }
+  const toggleMenu = () => {
+    let links = document.getElementById('links')
+    let nav = document.getElementById('nav')
 
-  state = {
-    "navText": "ls"
-  };
-
-  showLinks = () => {
-    let links = document.getElementById("links");
-    let nav = document.getElementById("nav");
-
-    if (links.style.display === "block") {
-      links.style.display = "none";
-      nav.style.height = "60px";
-      this.setState({"navText": "ls"})
+    if (links.style.display === 'block') {
+      closeMenu(links, nav)
     } else {
-      links.style.display = "block";
-      nav.style.height = "60vh";
-      this.setState({"navText": "cd"})
+      openMenu(links, nav)
     }
-
-    return false;
   }
 
-  closeMenu = () => {
-    let links = document.getElementById("links");
-    let nav = document.getElementById("nav");
-    links.style.display = "none";
-    nav.style.height = "60px";
-    this.setState({"navText": "ls"})
-    return false;
+  const openMenu = (links, nav) => {
+    links.style.display = 'block'
+    nav.style.height = '60vh';
+    setNavText('cd')
   }
 
-  render() {
-    return (
-      <nav id="nav">
-        <ul id="links" onClick={this.closeMenu}>
-          <li><a href="#home">~/</a></li>
-          <li><a href="#about">~/about</a></li>
-          <li><a href="#skills">~/skills</a></li>
-          <li><a href="#portfolio">~/portfolio</a></li>
-          <li><a href="#contact">~/contact</a></li>
-        </ul>
-  
-        <span className="icon" onClick={this.showLinks}>
-          {this.state.navText}
-        </span>
-      </nav>
-    );
+  const closeMenu = () => {
+    let links = document.getElementById('links')
+    let nav = document.getElementById('nav')
+
+    links.style.display = 'none'
+    nav.style.height = '60px';
+    setNavText('ls')
   }
+
+  return (
+    <nav id="nav">
+      <ul id="links" onClick={closeMenu}>
+        <li><a href="#home">~/</a></li>
+        <li><a href="#about">~/about</a></li>
+        <li><a href="#skills">~/skills</a></li>
+        <li><a href="#portfolio">~/portfolio</a></li>
+        <li><a href="#contact">~/contact</a></li>
+      </ul>
+
+      <span className="icon" onClick={toggleMenu}>
+        {navText}
+      </span>
+    </nav>
+  );
 }
