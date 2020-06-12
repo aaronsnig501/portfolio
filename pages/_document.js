7751d2f863e6d8631afba3c044725eea8eb6456a
Aaron Sinnott <aaronsnig@gmail.com>
2020-06-12 08:33:08 -0300

    Add Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,20 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta name="google-site-verification" content="O5Vmi27x6-HDjOjjN9xsWHNJFmJKNzXwvKvh9maKKDM" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-63068361-1"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `<!-- Global site tag (gtag.js) - Google Analytics -->
+                <script>
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+
+                  gtag('config', 'UA-63068361-1');
+                </script>
+                `,
+            }}
+          />
         </Head>
         <body>
           <Main />
