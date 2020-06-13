af0cb9304a5a3029a51082985d164059056c5fe1
Aaron Sinnott <aaronsnig@gmail.com>
2020-06-13 12:20:16 -0300

    Fix analytics issues

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,26 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
+
+  setGoogleTags() {
+    return {
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-63068361-2');
+      `
+    };
+  }
+
   render() {
     return (
       <Html lang="en">
         <Head>
           <script async src="https://www.googletagmanager.com/gtag/js?id=UA-63068361-2"></script>
           <script
-            dangerouslySetInnerHTML={{
-              __html: `<!-- Global site tag (gtag.js) - Google Analytics -->
-                <script>
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-
-                  gtag('config', 'UA-63068361-2');
-                </script>
-                `,
-            }}
+            dangerouslySetInnerHTML={this.setGoogleTags()}
           />
           <meta name="google-site-verification" content="O5Vmi27x6-HDjOjjN9xsWHNJFmJKNzXwvKvh9maKKDM" />
         </Head>
